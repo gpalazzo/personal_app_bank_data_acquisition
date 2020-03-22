57b5dd80e40389dd637531da88374b13ba0a8910
Guilherme Palazzo <35868223+gpalazzo@users.noreply.github.com>
2020-03-21 23:21:01 -0300

    deploy to json file

--- src/core/get_acc_balance.py
+++ src/core/get_acc_balance.py
@@ -1,8 +1,12 @@
 from wrapper_bank_initial_page import BankWrapper
 from datetime import datetime
 import json
+from pathlib import Path
+import io
+import os
 
 
+dir_path = Path(__file__).resolve().parents[2]
 chrome = BankWrapper().get_initial_page()
 
 try:
@@ -15,14 +19,11 @@ try:
     acc_balance_float = float(acc_balance_str)
 
     data_dict = {"time_check": str(datetime.now()),
-             "acc_balance": acc_balance_float,
-             "email_status": "not_sent"}
+                 "acc_balance": acc_balance_float,
+                 "email_status": "not_sent"}
 
-    json_dump_str = json.dumps(obj=data_dict)
-    json_loaded = json.loads(s=json_dump_str)
-
-    with open("output_files/acc_balance.json", "w") as f:
-        json.dump(obj=json_loaded, fp=f)
+    with io.open(os.path.join(dir_path, 'output_files/acc_balance.json'), 'w') as f:
+        f.write(json.dumps(data_dict))
 
 except Exception as e:
     print(f"Erro: {e.args}")
