46fc469bf57851268a2687bac9af32bcb1d2346d
Guilherme Palazzo <35868223+gpalazzo@users.noreply.github.com>
2020-03-23 01:03:29 -0300

    deleting unused directory

--- tests/clients/sending_email.py
+++
@@ -1,6 +0,0 @@
-from bank_microservice.src import GmailClient
-
-client = GmailClient()
-client.login()
-client.build_email_message(mail_objective="test", subject="test", body="python test")
-client.send_email()
