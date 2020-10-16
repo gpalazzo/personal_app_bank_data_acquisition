7d105cfd1b3676fab178989ecf53934335e0efa1
Guilherme Palazzo <palazzoeng1@gmail.com>
2020-10-15 22:39:44 -0300

    acquiring rdc post fixed data

--- src/core/banking/investments_balance.py
+++ src/core/banking/investments_balance.py
@@ -97,11 +97,15 @@ def get_rdc_lca_balance(chrome: Chrome):
 
     try:
 
-        rdc_xpath = '//*[@id="box_conteudo"]/table[2]/tbody/tr[1]/td[2]/font'
-        lca_xpath = '//*[@id="box_conteudo"]/table[2]/tbody/tr[2]/td[2]/font'
+        rdc_pre_fixed_xpath = '//*[@id="box_conteudo"]/table[2]/tbody/tr[1]/td[2]/font'
+        rdc_post_fixed_xpath = '//*[@id="box_conteudo"]/table[2]/tbody/tr[2]/td[2]/font'
+        lca_xpath = '//*[@id="box_conteudo"]/table[2]/tbody/tr[3]/td[2]/font'
+
         log_client.set_msg(log_type="info",
-                           log_msg=f"trying to reach elements at xpaths: {rdc_xpath} and {lca_xpath}")
-        rdc = chrome.find_element_by_xpath(rdc_xpath)
+                           log_msg=f"trying to reach elements at xpaths: {rdc_pre_fixed_xpath}, {rdc_post_fixed_xpath} and {lca_xpath}")
+
+        rdc_pre_fixed = chrome.find_element_by_xpath(rdc_pre_fixed_xpath)
+        rdc_post_fixed = chrome.find_element_by_xpath(rdc_post_fixed_xpath)
         lca = chrome.find_element_by_xpath(lca_xpath)
 
         log_client.set_msg(log_type="info",
@@ -109,10 +113,12 @@ def get_rdc_lca_balance(chrome: Chrome):
 
         log_client.set_msg(log_type="info",
                            log_msg="parsing elements text properties")
-        rdc_str = rdc.text.replace(".", "").replace(",", ".")
+
+        rdc_pre_fixed_str = rdc_pre_fixed.text.replace(".", "").replace(",", ".")
+        rdc_post_fixed_str = rdc_post_fixed.text.replace(".", "").replace(",", ".")
         lca_str = lca.text.replace(".", "").replace(",", ".")
 
-        return rdc_str, lca_str
+        return rdc_pre_fixed_str, rdc_post_fixed_str, lca_str
 
     except Exception as e:
 
@@ -133,7 +139,7 @@ def main():
 
         chrome = _get_chrome_authenticated()
 
-        rdc, lca = get_rdc_lca_balance(chrome=chrome)
+        rdc_pre_fixed, rdc_post_fixed, lca = get_rdc_lca_balance(chrome=chrome)
 
         time_now = datetime.now()
         date_today = datetime.strftime(time_now, "%Y-%m-%d")
@@ -141,7 +147,8 @@ def main():
         file_path = local_project_root_dir / Path(f"{FINANCIAL_DATA_FILES_OUTPUT_DIR}/bank_investments_balance_{date_today}.json")
 
         data_dict = {"action_timestamp": str(time_now),
-                     "rdc": rdc,
+                     "rdc_pre_fixed": rdc_pre_fixed,
+                     "rdc_post_fixed": rdc_post_fixed,
                      "lca": lca}
 
         if os.path.isfile(path=file_path) and os.access(file_path, os.R_OK):
